160b24ed5a26e3d84f1a052b7cbe8c8a08ddb9e6
hvsmrtn <mhavas16@student.aau.dk>
2019-03-06 13:58:26 +0100

    Add files via upload

---
+++ ply_read/ply_read/ply_read.cpp
@@ -0,0 +1,31 @@
+/*Way to read .ply file (original pig pointcloud) have the .ply 
+file in the same directory as your main.cpp, compile with cmake .. and make
+and then run the executable  */
+
+
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/io/ply_io.h>
+
+int
+main (int argc, char** argv)
+{
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+
+  if (pcl::io::loadPLYFile<pcl::PointXYZ> ("pigleg.ply", *cloud) == -1) //* load the file
+  {
+    PCL_ERROR ("Couldn't read file test_pcd.pcd \n");
+    return (-1);
+  }
+  std::cout << "Loaded "
+            << cloud->width * cloud->height
+            << " data points from test_pcd.pcd with the following fields: "
+            << std::endl;
+  for (size_t i = 0; i < cloud->points.size (); ++i)
+    std::cout << "    " << cloud->points[i].x
+              << " "    << cloud->points[i].y
+              << " "    << cloud->points[i].z << std::endl;
+
+  return (0);
+}
