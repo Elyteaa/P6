82d3cee0b40d0d4022e3a0405d88e81a2649d2bd
Elona <eloniuke@gmail.com>
2019-04-24 14:20:47 +0200

    Create intersectionFunction.cpp

---
+++ PCL_sandbox/intersectionFunction.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector <int> intersectingSegments(int longa, int longb, int shorta[], int shortb[], int size)
+{
+    vector <int> foundSegments;
+    for (int i = 0; i < size; i++)
+    {
+        //float I1 [2] = {min(cloud->points[longa].x, cloud->points[longb].x), max(cloud->points[longa].x, cloud->points[longb].x)};
+        //float I2 [2] = {min(cloud->points[i].x, cloud->points[i+1].x), max(cloud->points[i].x, cloud->points[i+1].x)};
+
+        float A1 = (cloud->points[longa].y - cloud->points[longb].y) / (cloud->points[longa].x - cloud->points[longb].x);
+        float A2 = (cloud->points[shorta[i]].y - cloud->points[shortb[i]].y) / (cloud->points[shorta[i]].x - cloud->points[shortb[i]].x);
+        float b1 = cloud->points[longa].y - A1 * cloud->points[longa].x;
+        float b2 = cloud->points[shorta[i]].y - A2 * cloud->points[shorta[i]].x;
+
+        if (A1 == A2) {continue;} //parallel segments
+
+        float Xa = (b2 - b1) / (A1 - A2);
+
+        if (Xa < max(min(cloud->points[longa].x, cloud->points[longb].x), min(cloud->points[shorta[i]].x), cloud->points[shortb[i]].x) || (Xa > min(max(cloud->points[longa].x, cloud->points[longb].x), max(cloud->points[shorta[i]].x), cloud->points[shortb[i]].x)))
+            {continue;} // Intersection out of bound
+        else {foundSegments.push_back(shorta[i]); foundSegments.push_back(shortb[i]);} //Every second element beginning/end of the line
+    }
+    return foundSegments;
+}
+
+int main()
+{
+    return 0;
+}
