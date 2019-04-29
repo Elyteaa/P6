4b2237fbe02558367a4fdeb101ea72762a61da2b
Andreas Nellemann Jørgensen <andr544e@gmail.com>
2019-04-29 13:13:44 +0200

    Update simplefind.cpp
    Lots of commented out stuff

--- PCL_sandbox/simplefind.cpp
+++ PCL_sandbox/simplefind.cpp
@@ -80,13 +80,13 @@ pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
 ///////////////////////////////////////////////////////////////////////////
 // Try out different pairs, looking for the longest distance between them
 const int size = cloud->points.size();
-float longest = 0.0;
-float len = 0.0;
+float longest = 0.0; // longest line found
+float len = 0.0;     // current length being evaluated
 float xdist = 0.0;
-float ydist = 0.0;
+float ydist = 0.0;   // distance between points in y-direction
 float zdist = 0.0;
-int idx1 = 0;
-int idx2 = 0;
+int idx1 = 0;        // one end of the line
+int idx2 = 0;        // other end of the line
 
 for (int i = 0; i < cloud->points.size()/2; i++)
 {
@@ -115,15 +115,15 @@ cout << "The longest line found goes between points [" << idx1 << "," << idx2
 // Placing the cut based on: thickness increase. Index increment
 const int gran = 1; //granularity, how many indices are skipped per jump
 const int iterations = cloud->points.size()/(gran*2);
-float shortest[iterations] = {1.0};
-int idx3[iterations] = {}; //nearest point to a randomized coord
-int idx4[iterations] = {}; //nearest point opposite from idx3
+float shortest[iterations] = {1.0}; // distances between pairs of idx3[i] and idx4[i]
+int idx3[iterations] = {};
+int idx4[iterations] = {}; //nearest point opposite from an idx3
 
-// populate idx3 with indices starting from idx1
+// populate idx3 with indices, starting from idx1
 for (int i = 0; i < iterations; i++)
 {
     if (idx1 + i*gran > cloud->points.size()-1)
-    {
+    {// in case we would exceed size of point cloud
         idx3[i] = idx1 + i*gran - cloud->points.size();
     }
     else
@@ -131,7 +131,6 @@ for (int i = 0; i < iterations; i++)
         idx3[i] = idx1 + i*gran;
     }
 }
-
 // find idx4 for each idx3
 for (int i = 0; i < iterations; i++)
 {
@@ -139,8 +138,8 @@ for (int i = 0; i < iterations; i++)
     {//if nearest point is close to the tip
         if (idx3[i]>idx1) //if nearest point is to the right
         {
-            if ((idx1-idx3[i])<0) //if the tip is close to idx(0)
-            {
+            if (((idx1-idx3[i])<10) || (cloud->points.size() - idx1)<10)
+            {//if the tip is close to idx(0)
                   idx4[i] = cloud->points.size() - (idx3[i] - idx1);
             }
             else
@@ -185,68 +184,228 @@ for (int i = 0; i < iterations; i++)
         }
     }
 }
+//////////////////////////////////////////////////////////////////////////////////
 // Disregard lines that don't cross longest line. Make new vector w/ only relevant
-std::vector<int> relevant;
+/*
+std::vector<int> relVec; //which lines are relevant
+int cross = 0; //How many are relevant
 for (int i = 0; i < iterations; i++)
 {
     if (intersectingSegments(idx1, idx2, idx3[i], idx4[i], cloud))
     {
-        relevant.push_back(i);
+        relVec.push_back(i+1);
+        cross++;
+    }
+    else
+    {
+        relVec.push_back(0);
     }
     //cout << "Line: " << i << " does: [" << maybe << "] cross" << endl; 
 }
-for (int i = 0; i < relevant.size(); i++)
+*/
+/*
+for (int i = 0; i < relVec.size(); i++)
 {
-    cout << "relevant: [" << relevant[i] << "]" << endl;
+    cout << "relVec: [" << relVec[i] << "]" << endl;
 }
+*/
 
-// Calculate average distance between idx3 and idx4 pairs
-float avg;
-float sum = 0.0;
-for (int i = 0; i < relevant.size(); i++)
-{
-    if (relevant[i])
-    {
-        /* code */
-    }
-    sum += shortest[i];
-}
-avg = sum/iterations;
-cout << "Average length across: " << avg << endl;
-
-////////////////////////////////////////////////////////////////////////////////
-// Placing the cut based on: distance from tip, 0.18m
-
-//Calculate angles 
+/////////////////////////////////////////////////////////////////////////////////
+// Discriminate lines based on angle // Calculate angles 
 Vector3f vec1, vec2;
 vec1 << cloud->points[idx2].x - cloud->points[idx1].x,
         cloud->points[idx2].y - cloud->points[idx1].y,
         cloud->points[idx2].z - cloud->points[idx1].z;
-cout << vec1 << endl;
+vec1 = vec1.normalized(); // vector version of the longest line (green)
 
-vec1 = vec1.normalized();
-cout << vec1 << endl;
-
-float angle;
+std::vector<int> relVec; //which lines are relevant
+uint cross = 0; //How many lines are valid
+float angle[iterations] = {}; //Angles of the lines
 for (int i = 0; i < iterations; i++)
 {
     vec2 << cloud->points[idx3[i]].x - cloud->points[idx4[i]].x,
             cloud->points[idx3[i]].y - cloud->points[idx4[i]].y,
             cloud->points[idx3[i]].z - cloud->points[idx4[i]].z;
-    vec2 = vec2.normalized();
+    vec2 = vec2.normalized(); //to ensure it has length 1
     
     float dotp = vec1.transpose() * vec2;
-    angle = acos(dotp) * 180/3.14159265; //angle in deg
+    angle[i] = acos(dotp) * 180/3.14159265; //angle in deg
 
-    cout << "angle " << i << " is: " << angle << endl;
+    if (abs(angle[i] - 90) < 30)
+    {
+        relVec.push_back(i+1); //positive indicates valid line
+        cross++; // count valid lines
+    }
+    else
+    {
+        relVec.push_back(0); // 0 indicates invalid line
+    }
+    cout << "angle " << i << " is: " << angle[i] << endl;
 }
+
+
+//Make new array with only the relevant lines
+float relShort[cross] = {};
+uint relcount = 0;
+for (int i = 0; i < iterations; i++)
+{
+    if (relVec[i])
+    {
+        relShort[relcount] = shortest[i];
+        relcount++;
+    }
+}
+
+for (int i = 0; i < cross; ++i)
+{
+    cout << "relShort [" << i << "] has value: " << relShort[i] << endl;
+}
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Evaluate thickness //
+// Simple version // Find local minimum, starting from the middle
+int minIdx = cross/2;
+bool minFound = 0;
+int check = 3;
+int dir = 0;
+while(!minFound)
+{
+    if (relShort[minIdx]>relShort[minIdx-1])
+    {
+        minIdx--;
+        dir = -1;
+    }
+    else if (relShort[minIdx]>relShort[minIdx+1])
+    {
+        minIdx++;
+        dir = 1;
+    }
+    else if (relShort[minIdx]<relShort[minIdx+1] && relShort[minIdx]<relShort[minIdx-1])
+    {
+        for (int i = 1; i <= check; i++)
+        {
+            if (relShort[minIdx + i*dir] < relShort[minIdx])
+            {
+                minIdx = minIdx + i*dir;
+                i = check + 1;
+                dir = 0;
+            }
+        }
+        if (dir)
+        {
+            minFound = true;
+        }
+        cout << "Found min: [" << minIdx << "] with value: [" << relShort[minIdx] << "]" << endl;
+        break;
+    }
+    else 
+    {
+        cout << "Error in finding minimum!" << endl;
+        break;
+    }
+}
+
+
+/*
+//Find 3 local minima
+int minIdx = cross/2;
+int minsFound = 0;
+int foundMins[3] = {};
+int minTries = 0;
+int searched[2] = {minIdx,minIdx}; //upper and lower bounds that have already been searched
+while(minsFound<3)
+{
+    if (minTries > 500) //if the program is stuck
+    {
+        cout << "The program is stuck" << endl;
+        break;
+    }
+    else if (relShort[minIdx]<relShort[minIdx+1] && relShort[minIdx]<relShort[minIdx-1])
+    {
+        foundMins[minsFound] = minIdx;
+        minsFound++;
+        minTries = 0;
+        cout << "Found min: [" << minIdx << "] with value: [" << relShort[minIdx] << "]" << endl;
+        if (minsFound == 1)
+        {
+            minIdx = cross * (3/4);
+        }
+        else
+        {
+            minIdx = cross * (1/4);
+        }
+    }
+    else if (relShort[minIdx]>relShort[minIdx-1])
+    {
+        minIdx--;
+        searched[0]--;
+        minTries++;
+    }
+    else if (relShort[minIdx]>relShort[minIdx+1])
+    {
+        minIdx++;
+        searched[1]++;
+        minTries++;
+    }
+    else
+    {
+        cout << "Error in finding minimum!" << endl;
+        break;
+    }
+}
+*/
+
+/*
+//Sorting approach: bubble
+int flag = 1;    // set flag to 1 to start first pass
+int temp;        // holding variable
+int numLength = num.length();
+for(int i = 1; (i <= cross) && flag; i++)
+{
+    flag = 0;
+    for (int j=0; j < (cross - 1); j++)
+    {
+        if (relShort[j+1] > relShort[j])      // ascending order simply changes to <
+        { 
+            temp = relShort[j];             // swap elements
+            num[j] = relShort[j+1];
+            relShort[j+1] = temp;
+            flag = 1;               // indicates that a swap occurred.
+        }
+    }
+}
+*/
+
+// Calculate average distance between idx3 and idx4 pairs
+float avg;
+float sum = 0.0;
+for (int i = 0; i < iterations; i++)
+{
+    if (relVec[i])
+    {
+        sum += shortest[i];
+    }
+}
+avg = sum/cross;
+cout << "Average length across: " << avg << endl;
+cout << cross << endl;
+
+////////////////////////////////////////////////////////////////////////////////
+// Placing the cut based on: distance from tip, 0.18m
 
 //coordinates where the cut should be
 float fix1, fix2;
-float cutCoord[3] = {};
-cutCoord[0] = cloud->points[idx2].x + vec1(0)*0;
-cutCoord[1] = cloud->points[idx2].y + vec1(1)*0;
-cutCoord[2] = cloud->points[idx2].z + vec1(2)*0;
+float cutCoord[3] = {0.0};
+float foo = 0;
+cutCoord[0] = cloud->points[idx1].x + vec1(0)*foo;
+cutCoord[1] = cloud->points[idx1].y + vec1(1)*foo;
+cutCoord[2] = cloud->points[idx1].z + vec1(2)*foo;
+
+for (int i = 0; i < 3; i++)
+{
+    cout << "cutCoord ["  << i << "]: " << cutCoord[i] << endl;
+}
 
 //nearest point on one side
 float cutWidth = 1.0;
@@ -263,11 +422,11 @@ for (int point = 0; point < cloud->points.size()/2; point++)
     }
 }
 
-//nearest point to that side
+//nearest point to that point, on the other side
 cutWidth = 1.0;
 for (int point = 0; point < cloud->points.size(); point++)
 {
-    if (abs(point-fix1)>30)
+    if (abs(point-fix1)>40) //exclude immediate neighbors
     {
         xdist = cloud->points[fix1].x - cloud->points[point].x;
         ydist = cloud->points[fix1].y - cloud->points[point].y;
@@ -275,31 +434,38 @@ for (int point = 0; point < cloud->points.size(); point++)
         len = sqrt(pow(xdist, 2.0) + pow(ydist, 2.0) + pow(zdist, 2.0));
 
         if (len<cutWidth)
-            {
-                cutWidth = len;
-                fix2 = point;
-            }
+        {
+            cutWidth = len;
+            fix2 = point;
+        }
     }
 }
 cout << "Cut at fixed 18cm has width: " << cutWidth << endl;
 cout << "Fixed cut has idxs: [" << fix1 << ", " << fix2 << "]" << endl;
+cout << "fix1 has coord x: " <<  cloud->points[fix1].x << endl;
+cout << "fix1 has coord y: " <<  cloud->points[fix1].y << endl;
+cout << "fix1 has coord z: " <<  cloud->points[fix1].z << endl;
 
 ///////////////////////////////////////////////////////////////////////////////
+// Viewer //
 pcl::visualization::PCLVisualizer viewer("PCL Viewer");
 viewer.setBackgroundColor (0, 0, 0);
 viewer.addPointCloud<pcl::PointXYZ> (cloud, "sample cloud");
 
 for(int j = 0; j < iterations; j++) //idx-wise lines across the leg
 {
-      stringstream ss;
-      ss << j;
-      string str = ss.str();
-      viewer.addLine(cloud->points[idx3[j]], cloud->points[idx4[j]], 1, 0, 0, str); //between points
+    stringstream ss;
+    ss << j;
+    string str = ss.str();
+    //if (relVec[j])
+    //{
+        viewer.addLine(cloud->points[idx3[j]], cloud->points[idx4[j]], 1, 0, 0, str); //between points
+    //}
 }
 
 viewer.addPointCloudNormals<pcl::PointXYZ,pcl::Normal>(cloud,normals);
 viewer.addLine(cloud->points[idx1], cloud->points[idx2], 0, 1, 0, "q"); //Show longest line
-viewer.addLine(cloud->points[fix1], cloud->points[fix2], 1, 1, 0, "fixedLength"); //18cm line
+//viewer.addLine(cloud->points[fix1], cloud->points[fix2], 1, 1, 0, "fixedLength"); //18cm line
 
 while(!viewer.wasStopped ())
 {
@@ -309,11 +475,28 @@ return 0;
 }
 
 /*
-for(int i=0; i < cloud->points.size()-1; i++)
+//Simple version
+//Find local minimum, starting from the middle
+int minIdx = cross/2;
+bool minFound = 0;
+while(!minFound)
 {
-      stringstream ss;
-      ss << i;
-      string str = ss.str();
-      viewer.addLine(cloud->points[i], cloud->points[i+1], 1, 1, 1, str); //Make lines between points of the leg
+    if (relShort[minIdx]<relShort[minIdx+1] && relShort[minIdx]<relShort[minIdx-1])
+    {
+        minFound = true;
+        cout << "Found min: [" << minIdx << "] with value: [" << relShort[minIdx] << "]" << endl;
+    }
+    else if (relShort[minIdx]>relShort[minIdx-1])
+    {
+        minIdx--;
+    }
+    else if (relShort[minIdx]>relShort[minIdx+1])
+    {
+        minIdx++;
+    }
+    else
+    {
+        cout << "Error in finding minimum!" << endl;
+    }
 }
 */
