613353f914a6d3bfc14df86e28a5cbfed27a9bcf
mrosssi16 <mrossi16@student.aau.dk>
2019-04-30 14:18:52 +0200

    Update 18cm>_>.cpp

--- PCL_sandbox/18cm>_>.cpp
+++ PCL_sandbox/18cm>_>.cpp
@@ -1,128 +1,132 @@
-////////////////////////////////////////////////////////////////////////////////
-// Placing the cut based on: distance from tip, 0.18m
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <iostream>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/common/io.h>
+#include <vector>
+#include <tuple>
 
-//coordinates where the cut should be
-float fix1, fix2;
-float cutCoord[3] = {0.0};
-float foo = 0;
-cutCoord[0] = cloud->points[idx1].x + vec1(0)*foo;
-cutCoord[1] = cloud->points[idx1].y + vec1(1)*foo;
-cutCoord[2] = cloud->points[idx1].z + vec1(2)*foo;
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/segmentation/extract_clusters.h>
 
-for (int i = 0; i < 3; i++)
+#include <pcl/filters/project_inliers.h>
+#include <pcl/filters/passthrough.h>
+
+using namespace std;
+
+tuple<int, int> eighteen (pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int start, int end);
+
+tuple<int, int> eighteen (pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int start, int end)
 {
-    cout << "cutCoord ["  << i << "]: " << cutCoord[i] << endl;
-}
 
-//nearest point on one side
-float cutWidth = 0;
-for (int point = 0; point < cloud->points.size()/2; point++)
-{
-    xdist = cutCoord[0] - cloud->points[point].x;
-    ydist = cutCoord[1] - cloud->points[point].y;
-    zdist = cutCoord[2] - cloud->points[point].z;
-    len = sqrt(pow(xdist, 2.0) + pow(ydist, 2.0) + pow(zdist, 2.0));
-    cout << "x = " << xdist << " y = " << ydist << " z = " << zdist << endl;
-    cout << "length = " << len << endl;
-    if (len>cutWidth)
-    {
-        fix1 = point;
-        cout << "fix 1 = " << fix1 << endl;
-    }
-}
-
-double distx = cloud->points[idx1].x - cloud->points[idx2].x;
-double disty = cloud->points[idx1].y - cloud->points[idx2].y;
-double distz = cloud->points[idx1].z - cloud->points[idx2].z;
+double distx = cloud->points[start].x - cloud->points[end].x;
+double disty = cloud->points[start].y - cloud->points[end].y;
+double distz = cloud->points[start].z - cloud->points[end].z;
 //Finding the length of the line
 double len1 = sqrt(pow(distx, 2.0) + pow(disty, 2.0) + pow(distz, 2.0));
-cout << "old length = " << len1 << endl;
+
 double difflen = 0.14/len1;
-cout << "differnece = " << difflen << endl;
+
 //Scaling the line to 0.18
 len1 = len1 * difflen;
-cout << "New length = " << len1 << endl;
-
-cout << " x = " << distx << "  y = " << disty << "  z = " << distz << endl;
-
-//Scaling the distances -> It seems to be a stupid way to do this and thus should probably just be deleted
-distx = distx * difflen;
-disty = disty * difflen;
-distz = distz * difflen;
-
-cout << "new x = " << distx << " new y = " << disty << " new z = " << distz << endl;
 
 //By modifying t we can find any point on the line
 //where t = 0 corresponds to our starting point and t = 1 to our end point
 //any value of t in between will be a point on the line between start and end
 double t = -1 * difflen;
 //Here we calculate the direction "vector"
-double directionx = cloud->points[idx1].x - cloud->points[idx2].x;
-double directiony = cloud->points[idx1].y - cloud->points[idx2].y;
-double directionz = cloud->points[idx1].z - cloud->points[idx2].z;
+double directionx = cloud->points[start].x - cloud->points[end].x;
+double directiony = cloud->points[start].y - cloud->points[end].y;
+double directionz = cloud->points[start].z - cloud->points[end].z;
 
 
 //Pushing the point on the line 18cm from start to the pointcloud
 //Notice that each point is calculated by adding the start point with the direction vector multiplied by t
 pcl::PointXYZ pp;
-pp.x = cloud->points[idx1].x + directionx * t; pp.y = cloud->points[idx1].y + directiony * t; pp.z = cloud->points[idx1].z + directionz * t;
+pp.x = cloud->points[start].x + directionx * t; pp.y = cloud->points[start].y + directiony * t; pp.z = cloud->points[start].z + directionz * t;
 cloud->push_back(pp);
 
 cout << cloud->points.size() << endl;
 //Here we find the distance from the start of the line to the point
 int lastindex = cloud->points.size()-1;
-double distpointx = cloud->points[idx1].x - cloud->points[lastindex].x;
-double distpointy = cloud->points[idx1].y - cloud->points[lastindex].y;
-double distpointz = cloud->points[idx1].z - cloud->points[lastindex].z;
+double distpointx = cloud->points[start].x - cloud->points[lastindex].x;
+double distpointy = cloud->points[start].y - cloud->points[lastindex].y;
+double distpointz = cloud->points[start].z - cloud->points[lastindex].z;
 double lenToPoint = sqrt(pow(distpointx, 2.0) + pow(distpointy, 2.0) + pow(distpointz, 2.0));
 double lenToPoint1 = lenToPoint;
 //Here we search for the smallest distance to the 18cm point from any point in the pointclooud
 //The shortst distance to the point will then be the last i value -1 since the smallest distance to the point is the distance from the point to the itself
-
+int p1 = 0;
+int p2 = 0;
+vector<int> temp;
 for(int i = 0; i < cloud->points.size(); i++)
 {
-	double itepointx = cloud->points[i].x - cloud->points[lastindex].x;
-	double itepointy = cloud->points[i].y - cloud->points[lastindex].y;
-	double itepointz = cloud->points[i].z - cloud->points[lastindex].z;
+  double itepointx = cloud->points[i].x - cloud->points[lastindex].x;
+  double itepointy = cloud->points[i].y - cloud->points[lastindex].y;
+  double itepointz = cloud->points[i].z - cloud->points[lastindex].z;
 
-	if(lenToPoint >= sqrt(pow(itepointx, 2.0) + pow(itepointy, 2.0) + pow(itepointz, 2.0)))
-		{
-			lenToPoint = sqrt(pow(itepointx, 2.0) + pow(itepointy, 2.0) + pow(itepointz, 2.0));
-			cout << " i = " << i << endl;
-
-		//cout << "i = " << i << "x = " << minx << " y = " << miny << " z = " << minz << endl;
-		}
+  if(lenToPoint != 0 && lenToPoint > sqrt(pow(itepointx, 2.0) + pow(itepointy, 2.0) + pow(itepointz, 2.0)))
+    {
+      lenToPoint = sqrt(pow(itepointx, 2.0) + pow(itepointy, 2.0) + pow(itepointz, 2.0));
+      cout << " i = " << i << endl;
+      temp.push_back(i);
+      p1 = temp.rbegin()[1];
+    }
 }
-cout << "g" << endl;
 for(int i = 0; i < cloud->points.size()/2; i++)
 {
-	double itepointx = cloud->points[i].x - cloud->points[lastindex].x;
-	double itepointy = cloud->points[i].y - cloud->points[lastindex].y;
-	double itepointz = cloud->points[i].z - cloud->points[lastindex].z;
+  double itepointx = cloud->points[i].x - cloud->points[lastindex].x;
+  double itepointy = cloud->points[i].y - cloud->points[lastindex].y;
+  double itepointz = cloud->points[i].z - cloud->points[lastindex].z;
 
-	if(lenToPoint1 > sqrt(pow(itepointx, 2.0) + pow(itepointy, 2.0) + pow(itepointz, 2.0)))
-		{
-			lenToPoint1 = sqrt(pow(itepointx, 2.0) + pow(itepointy, 2.0) + pow(itepointz, 2.0));
-			cout << " i1 = " << i << endl;
-
-		//cout << "i = " << i << "x = " << minx << " y = " << miny << " z = " << minz << endl;
-}
-}
-//nearest point to that point, on the other side
-cutWidth = 1.0;
-for (int point = 0; point < cloud->points.size(); point++)
-{
-    if (abs(point-fix1)>40) //exclude immediate neighbors
+  if(lenToPoint1 > sqrt(pow(itepointx, 2.0) + pow(itepointy, 2.0) + pow(itepointz, 2.0)))
     {
-        xdist = cloud->points[fix1].x - cloud->points[point].x;
-        ydist = cloud->points[fix1].y - cloud->points[point].y;
-        zdist = cloud->points[fix1].z - cloud->points[point].z;
-        len = sqrt(pow(xdist, 2.0) + pow(ydist, 2.0) + pow(zdist, 2.0));
+      lenToPoint1 = sqrt(pow(itepointx, 2.0) + pow(itepointy, 2.0) + pow(itepointz, 2.0));
+      cout << " i1 = " << i << endl;
+      p2 = i;
+}
+}
+return make_tuple(p1, p2);
 
-        if (len<cutWidth)
-        {
-            cutWidth = len;
-            fix2 = point;
-        }
-    }
+}
+
+int main(int argc, char** argv)
+{
+
+pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+pcl::PointCloud<pcl::PointXYZ>::Ptr plane(new pcl::PointCloud<pcl::PointXYZ>);
+pcl::PointCloud<pcl::PointXYZ>::Ptr plane_out(new pcl::PointCloud<pcl::PointXYZ>);
+
+  pcl::io::loadPCDFile <pcl::PointXYZ>("concaveboi.pcd", *cloud);
+
+
+int point1;
+int point2;
+//Replace 1 and 94 with the variables which hold the index values for the start and end of the longest line.
+//idx1 and idx2
+tie(point1, point2) = eighteen(cloud, 1, 94);
+cout << "First point = " << point1 << " Second point = " << point2 << endl;
+int lastindex = cloud->points.size()-1;
+cout << lastindex << endl;
+
+
+  pcl::visualization::PCLVisualizer viewer("PCL Viewer");
+  viewer.setBackgroundColor(0.0, 0.0, 0.0);
+  viewer.addPointCloud<pcl::PointXYZ>(cloud, "sample plane two");
+  viewer.addCoordinateSystem(0.1);
+  viewer.addLine(cloud->points[point1], cloud->points[lastindex], 0, 0, 1, "t");
+  viewer.addLine(cloud->points[point2], cloud->points[lastindex], 0, 1, 0, "rt");
+
+  while (!viewer.wasStopped())
+  {
+    viewer.spinOnce();
+  }
+
+  return 0;
 }
