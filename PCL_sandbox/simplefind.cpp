b3d330459a5adc6573caa1eb0aeeba758a80a308
Andreas Nellemann Jørgensen <andr544e@gmail.com>
2019-05-01 09:35:33 +0200

    Update simplefind.cpp

--- PCL_sandbox/simplefind.cpp
+++ PCL_sandbox/simplefind.cpp
@@ -21,6 +21,119 @@
 using namespace std;
 using namespace Eigen;
 
+class Pose {
+private:
+    float position[3];
+    float orientation[3];
+
+public:
+    
+    void set_values(){
+        cout << "hello class" << endl;
+    }
+};
+
+tuple<int, int> eighteen (pcl::PointCloud<pcl::PointXYZ>::Ptr cloud, int start, int end)
+{
+
+double distx = cloud->points[start].x - cloud->points[end].x;
+double disty = cloud->points[start].y - cloud->points[end].y;
+double distz = cloud->points[start].z - cloud->points[end].z;
+//Finding the length of the line
+double len1 = sqrt(pow(distx, 2.0) + pow(disty, 2.0) + pow(distz, 2.0));
+
+double difflen = 0.14/len1;
+
+//Scaling the line to 0.18
+len1 *= difflen;
+
+//By modifying t we can find any point on the line
+//where t = 0 corresponds to our starting point and t = 1 to our end point
+//any value of t in between will be a point on the line between start and end
+double t = -1 * difflen;
+//Here we calculate the direction "vector"
+double directionx = cloud->points[start].x - cloud->points[end].x;
+double directiony = cloud->points[start].y - cloud->points[end].y;
+double directionz = cloud->points[start].z - cloud->points[end].z;
+
+
+//Pushing the point on the line 18cm from start to the pointcloud
+//Notice that each point is calculated by adding the start point with the direction vector multiplied by t
+pcl::PointXYZ pp;
+pp.x = cloud->points[start].x + directionx * t; pp.y = cloud->points[start].y + directiony * t; pp.z = cloud->points[start].z + directionz * t;
+cloud->push_back(pp);
+
+cout << cloud->points.size() << endl;
+//Here we find the distance from the start of the line to the point
+int lastindex = cloud->points.size()-1;
+double distpointx = cloud->points[start].x - cloud->points[lastindex].x;
+double distpointy = cloud->points[start].y - cloud->points[lastindex].y;
+double distpointz = cloud->points[start].z - cloud->points[lastindex].z;
+double lenToPoint = sqrt(pow(distpointx, 2.0) + pow(distpointy, 2.0) + pow(distpointz, 2.0));
+double lenToPoint1 = lenToPoint;
+//Here we search for the smallest distance to the 18cm point from any point in the pointclooud
+//The shortst distance to the point will then be the last i value -1 since the smallest distance to the point is the distance from the point to the itself
+int p1 = 0;
+int p2 = 0;
+vector<int> temp;
+for(int i = 0; i < cloud->points.size(); i++)
+{
+    double itepointx = cloud->points[i].x - cloud->points[lastindex].x;
+    double itepointy = cloud->points[i].y - cloud->points[lastindex].y;
+    double itepointz = cloud->points[i].z - cloud->points[lastindex].z;
+
+    if(lenToPoint > sqrt(pow(itepointx, 2.0) + pow(itepointy, 2.0) + pow(itepointz, 2.0)))
+    {
+        lenToPoint = sqrt(pow(itepointx, 2.0) + pow(itepointy, 2.0) + pow(itepointz, 2.0));
+        temp.push_back(i);
+        p1 = temp.rbegin()[1];
+    }
+}
+for(int i = 0; i < cloud->points.size(); i++)
+{
+    if (abs(p1-i)>30)//exclude immediate neighbors
+    {
+        double itepointx = cloud->points[i].x - cloud->points[p1].x;
+        double itepointy = cloud->points[i].y - cloud->points[p1].y;
+        double itepointz = cloud->points[i].z - cloud->points[p1].z;
+
+        if(lenToPoint1 > sqrt(pow(itepointx, 2.0) + pow(itepointy, 2.0) + pow(itepointz, 2.0)))
+        {
+            lenToPoint1 = sqrt(pow(itepointx, 2.0) + pow(itepointy, 2.0) + pow(itepointz, 2.0));
+            p2 = i;
+        }
+    }
+}
+return make_tuple(p1, p2);
+}
+
+
+bool startIsNarrower(float myArray[], int arraySize){
+//Checks if the first half of an array has a smaller mean value than the 2nd half
+float sum = 0.0, avg1, avg2;
+    for (int i = 0; i < arraySize/2; i++)
+    {
+        sum += myArray[i];
+    }
+    avg1 = sum/arraySize/2;
+
+    sum = 0.0;
+    for (int i = arraySize/2; i < arraySize; i++)
+    {
+        sum += myArray[i];
+    }
+    cout << "hello function" << endl;
+
+    avg2 = sum/arraySize/2;
+
+    if (avg1<avg2)
+    {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 int main (int argc, char** argv) {
 srand (static_cast <unsigned> (time(0)));
 // load point cloud
@@ -86,7 +199,7 @@ cout << "The longest line found goes between points [" << idx1 << "," << idx2
 // Placing the cut based on: thickness increase. Index increment
 const int gran = 1; //granularity, how many indices are skipped per jump
 const int iterations = cloud->points.size()/(gran*2);
-float shortest[iterations] = {1.0}; // distances between pairs of idx3[i] and idx4[i]
+float shortest[iterations] = {1.0}; //distances between pairs of idx3[i] and idx4[i]
 int idx3[iterations] = {};
 int idx4[iterations] = {}; //nearest point opposite from an idx3
 
@@ -156,6 +269,16 @@ for (int i = 0; i < iterations; i++)
     }
 }
 /////////////////////////////////////////////////////////////////////////////////
+// Make sure that idx1 is the narrow end
+if (startIsNarrower(shortest, iterations))
+{
+    cout << "idx1 is already in the narrow end of the leg" << endl;
+} else {
+    int temp = idx1; // switch them
+    idx1 = idx2;
+    idx2 = temp;
+}
+/////////////////////////////////////////////////////////////////////////////////
 // Discriminate lines based on angle // Calculate angles
 Vector3f vec1, vec2;
 vec1 << cloud->points[idx2].x - cloud->points[idx1].x,
@@ -176,7 +299,7 @@ for (int i = 0; i < iterations; i++)
     float dotp = vec1.transpose() * vec2;
     angle[i] = acos(dotp) * 180/3.14159265; //angle in deg
 
-    if (abs(angle[i] - 90) < 35) //accept a diff of up to x deg
+    if (abs(angle[i] - 90) < 35) //accept a diff of up to x deg // WEIRD ERROR WITH LOW VALUE
     {
         relVec.push_back(i+1); //positive indicates valid line
         valCount++; // count valid lines
@@ -208,11 +331,11 @@ for (int i = 0; i < valCount; ++i)
 ///////////////////////////////////////////////////////////////////////////////
 // Evaluate thickness //
 // Find local minimum, starting from the middle
-uint minIdx = valCount/2 + 10;
-bool minFound = 0;
+uint minIdx = valCount/2;
+bool minFound = false;
 uint minTries = 0;
-uint check = 5;
-int dir = 1;
+uint check = valCount/12; //Check a nr of steps, proportional to size of array
+int dir = 1; //is pos to ensure that it can move, if it starts in a local minimum
 while(!minFound)
 {
     if (relShort[minIdx]>relShort[minIdx-1])
@@ -277,69 +400,33 @@ for (int i = 0; i < iterations; i++)
 
 cout << "Min has original idx: [" << finIdx << "] with value: [" << shortest[finIdx] << "]" << endl;
 
-// Output concluded end effector pose
-
-class Pose {
-    float position[3];
-    float orientation[3];
-public:
-    set_values()
-}
-
-
 ////////////////////////////////////////////////////////////////////////////////
 // Placing the cut based on: distance from tip, 0.18m
 //coordinates where the cut should be
-float fix1, fix2;
-float cutCoord[3] = {0.0};
-float foo = 0;
-cutCoord[0] = cloud->points[idx1].x + vec1(0)*foo;
-cutCoord[1] = cloud->points[idx1].y + vec1(1)*foo;
-cutCoord[2] = cloud->points[idx1].z + vec1(2)*foo;
+int point1;
+int point2;
+//Replace 1 and 94 with the variables which hold the index values for the start and end of the longest line.
+//idx1 and idx2
+tie(point1, point2) = eighteen(cloud, idx1, idx2);
+cout << "First point = " << point1 << " Second point = " << point2 << endl;
+int lastindex = cloud->points.size()-1;
+cout << lastindex << endl;
 
-for (int i = 0; i < 3; i++)
-{
-    cout << "cutCoord ["  << i << "]: " << cutCoord[i] << endl;
-}
 
-//nearest point on one side
-float cutWidth = 1.0;
-for (int point = 0; point < cloud->points.size()/2; point++)
-{
-    xdist = cutCoord[0] - cloud->points[point].x;
-    ydist = cutCoord[1] - cloud->points[point].y;
-    zdist = cutCoord[2] - cloud->points[point].z;
-    len = sqrt(pow(xdist, 2.0) + pow(ydist, 2.0) + pow(zdist, 2.0));
+/////////////////////////////////////////////////////////////////////////////////
+// Combine the two estimates
 
-    if (len<cutWidth)
-    {
-        fix1 = point;
-    }
-}
 
-//nearest point to that point, on the other side
-cutWidth = 1.0;
-for (int point = 0; point < cloud->points.size(); point++)
-{
-    if (abs(point-fix1)>40) //exclude immediate neighbors
-    {
-        xdist = cloud->points[fix1].x - cloud->points[point].x;
-        ydist = cloud->points[fix1].y - cloud->points[point].y;
-        zdist = cloud->points[fix1].z - cloud->points[point].z;
-        len = sqrt(pow(xdist, 2.0) + pow(ydist, 2.0) + pow(zdist, 2.0));
 
-        if (len<cutWidth)
-        {
-            cutWidth = len;
-            fix2 = point;
-        }
-    }
-}
-cout << "Cut at fixed 18cm has width: " << cutWidth << endl;
-cout << "Fixed cut has idxs: [" << fix1 << ", " << fix2 << "]" << endl;
-cout << "fix1 has coord x: " <<  cloud->points[fix1].x << endl;
-cout << "fix1 has coord y: " <<  cloud->points[fix1].y << endl;
-cout << "fix1 has coord z: " <<  cloud->points[fix1].z << endl;
+
+///////////////////////////////////////////////////////////////////////////////
+// Output concluded end effector pose
+
+
+
+Pose myPose;
+myPose.set_values();
+
 
 ///////////////////////////////////////////////////////////////////////////////
 // Viewer //
@@ -356,7 +443,7 @@ for(int j = 0; j < iterations; j++) //idx-wise lines across the leg
     {
         if (j == finIdx) //concluded index
         {
-            viewer.addLine(cloud->points[idx3[j]], cloud->points[idx4[j]], 0, 1, 0, str); //concluded line
+            viewer.addLine(cloud->points[idx3[j]], cloud->points[idx4[j]], 1, 1, 0, str); //concluded line
         }
         else
         {
@@ -367,7 +454,7 @@ for(int j = 0; j < iterations; j++) //idx-wise lines across the leg
 
 viewer.addPointCloudNormals<pcl::PointXYZ,pcl::Normal>(cloud,normals);
 viewer.addLine(cloud->points[idx1], cloud->points[idx2], 0, 1, 0, "q"); //Show longest line
-//viewer.addLine(cloud->points[fix1], cloud->points[fix2], 1, 1, 0, "fixedLength"); //18cm line
+viewer.addLine(cloud->points[point1], cloud->points[point2], 0, 1, 0, "t"); //18cm line
 
 while(!viewer.wasStopped ())
 {
